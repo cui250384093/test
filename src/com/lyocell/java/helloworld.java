e268dda2cf71a2119f67f71eb0a2e75eb571a16b
cui250384093 <cui250384093@163.com>
2020-04-20 08:48:42 +0800

    Initial commit

---
+++ src/com/lyocell/java/helloworld.java
@@ -0,0 +1,11 @@
+package com.lyocell.java;
+
+/**
+ * @author candk
+ * @create 2020-04-20-8:26 AM
+ **/
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
